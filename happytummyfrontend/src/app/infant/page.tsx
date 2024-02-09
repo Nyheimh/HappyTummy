dcfa41163ccd392ca4915ade59c037a33f0077bc
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-02-08 16:19:53 -0800

    Bones of application created

---
+++ happytummyfrontend/src/app/infant/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RecipeCard from "../ui/RecipeCard";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <RecipeCard />
+    </div>
+  );
+};
+
+export default page;
---
+++ happytummyfrontend/src/app/lib/NavBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box } from "@mui/material";
+
+import React from "react";
+
+const NavBar = () => {
+  const classes = useStyles();
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "Infant",
+    },
+    {
+      id: 3,
+      link: "Adult",
+    },
+  ];
+  return (
+    <Box className={classes.navBarContainer}>
+      <ul>
+        {links.map(({ id, link }) => (
+          <li key={id}>
+            <Link href={link}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+    </Box>
+  );
+};
+
+export default NavBar;
