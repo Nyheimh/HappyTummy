2ef30d1a0fad4670a08318a54e9d774bdf1463da
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-02-18 21:44:33 -0800

    Learning and implementing Nest.js

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsAlphanumeric } from 'class-validator'
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsAlphanumeric()
+  name: string
+
+}
