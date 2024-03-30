aad12e5fcd711e19547addf16f99e09e9c01aeaf
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-03-29 22:11:29 -0700

    Infant select option

--- src/app/infant/page.tsx
+++ src/app/infant/page.tsx
@@ -1,12 +1,26 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import RecipeCard from "../ui/RecipeCard";
-import { Box, Input, Stack, SimpleGrid } from "@chakra-ui/react";
+import { Box, Input, Stack, SimpleGrid, Select } from "@chakra-ui/react";
 import useInfantRecipeData from "./InfantRecipeData";
 
 const Page: React.FC = () => {
   const { searchFilter, handleSearchInputChange, filteredRecipes } =
     useInfantRecipeData();
+
+  const [sortOption, setSortOption] = useState("A-Z");
+
+  const handelSortChange = (option: any) => {
+    setSortOption(option);
+  };
+
+  const sortedRecipes = [...filteredRecipes].sort((a, b) => {
+    if (sortOption === "A-Z") {
+      return a.name.localeCompare(b.name);
+    } else {
+      return b.name.localeCompare(a.name);
+    }
+  });
   return (
     <Box>
       <Input
@@ -22,10 +36,19 @@ const Page: React.FC = () => {
           margin: 10,
         }}
       />
+      <Select
+        value={sortOption}
+        onChange={(e) => handelSortChange(e.target.value)}
+        style={{ width: 200, margin: 10 }}
+      >
+        <option value="A-Z"> Alphabetical (A-Z)</option>
+        <option value="Z-A">Alphabetical (Z-A)</option>
+      </Select>
+
       <Box style={{ display: "flex", justifyContent: "space-evenly" }}>
         <Stack spacing="3" direction="row" align="center">
           <SimpleGrid columns={3} spacing={5}>
-            {filteredRecipes.map((recipe) => (
+            {sortedRecipes.map((recipe) => (
               <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
           </SimpleGrid>
